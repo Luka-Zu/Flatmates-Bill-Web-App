b492c1dba0f62c2506537f8163a28db8183c8cca
LukaZukhbaia <95714793+LukaZukhbaia@users.noreply.github.com>
2022-08-18 00:05:26 +0400

    .

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask.views import MethodView
-from wtforms import Form
+from wtforms import Form, StringField, SubmitField
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -9,21 +9,33 @@ class HomePage(MethodView):
     def get(self):
         return render_template('index.html')
 
+class BillForm(Form):
+    amount = StringField("Bill amount: ")
+    period = StringField("Bill period: ")
+
+    name1 = StringField("Name: ")
+    days_in_house1 = StringField("Days in the house: ")
+    name2 = StringField("Name: ")
+    days_in_house2 = StringField("Days in the house: ")
+
+    button = SubmitField("Calculate")
 
 class BillFormPage(MethodView):
+
     def get(self):
-        return "BILL PAGE"
+        bill_form = BillForm()
+        return render_template('bill_form_page.html',
+                               billform=bill_form)
 
 
 class ResultsPage(MethodView):
     pass
 
 
-class BillForm(Form):
-    pass
 
 
 app.add_url_rule('/', view_func=HomePage.as_view('home_page'))
-app.add_url_rule('/bill_form_page', view_func=BillFormPage.as_view('bill_form_page'))
+app.add_url_rule('/bill_form', view_func=BillFormPage.as_view('bill_form'))
 
 app.run(debug=True)
+
