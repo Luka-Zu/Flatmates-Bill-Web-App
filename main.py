ed4b0a00f3ee08613656190ab8463579d5eb3f19
LukaZukhbaia <95714793+LukaZukhbaia@users.noreply.github.com>
2022-08-21 06:45:50 +0400

    .

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask.views import MethodView
 from wtforms import Form, StringField, SubmitField
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -8,6 +8,7 @@ app = Flask(__name__)
 class HomePage(MethodView):
     def get(self):
         return render_template('index.html')
+
 
 class BillForm(Form):
     amount = StringField("Bill amount: ")
@@ -20,6 +21,7 @@ class BillForm(Form):
 
     button = SubmitField("Calculate")
 
+
 class BillFormPage(MethodView):
 
     def get(self):
@@ -29,13 +31,16 @@ class BillFormPage(MethodView):
 
 
 class ResultsPage(MethodView):
-    pass
 
-
+    def post(self):
+        billform = BillForm(request.form)
+        amount = billform.amount.data
+        return amount
 
 
 app.add_url_rule('/', view_func=HomePage.as_view('home_page'))
 app.add_url_rule('/bill_form', view_func=BillFormPage.as_view('bill_form'))
+app.add_url_rule('/results', view_func=ResultsPage.as_view('results_page'))
+
 
 app.run(debug=True)
-
